2375f2248f3932627ef84358fd9413b0c484e33e
aisya037 <63236082+aisya037@users.noreply.github.com>
2020-04-16 22:33:23 +0700

    Update main_ngram.c

--- main_ngram.c
+++ main_ngram.c
@@ -20,7 +20,6 @@ struct data{
     char text[char_max];
     char next[20][char_max];
     int id_count;
-
 };
 
 struct string{
@@ -75,7 +74,6 @@ void pemecah_kata(double n, FILE *fp, struct string *trans_array,struct data *un
       strcpy((*(unique_array+h)).text,temp);
       strcpy((*(unique_array+h)).next[0],next_temp);
       (*(unique_array+h)).id_count = 1;
-
       h+=1;
     }
     else{
@@ -90,22 +88,46 @@ void pemecah_kata(double n, FILE *fp, struct string *trans_array,struct data *un
           (*(unique_array+z)).id_count += 1;
           printf("%d elemen ke %d : %s\n",z,(k+1),(*(unique_array+z)).next[k+1]);
           printf("%d elemen ke %d : %d\n",z,(k+1),(*(unique_array+z)).id_count);
-          printf("%d elemen ke %d : %s\n",z,(k+1),(*(unique_array+z)).next[k+1]);
         }
       }
       //printf("5\n");
     }
     j+=1;
   }
-  
-  printf("%s\n",(*(unique_array+0)).next[0]);
-  printf("%s\n",(*(unique_array+0)).next[1]);
-  printf("%d\n",(*(unique_array+0)).id_count);
-	//inicuma buat testing
-  printf("%s\n",(*(unique_array+0)).text);
-  printf("%s\n",(*(unique_array+0)).next[0]);
-  printf("%s\n",(*(unique_array+0)).next[1]);
+
+  printf("1: %s\n",(*(unique_array+0)).next[0]);
+  printf("2: %s\n",(*(unique_array+0)).next[1]);
+  printf("3: %s\n",(*(unique_array+0)).next[2]);
+  printf("4: %s\n",(*(unique_array+0)).next[4]);
+
+
+  printf("jumlah value : %d\n",(*(unique_array+0)).id_count);
   fclose(fp);
+}
+
+void tabelngram(struct data *unique_array){
+    int i=0, next_count = -1; char value[99];
+    printf("\t\t\t***Tabel n gram***\n\n");
+    printf("%200s%20.200s\n","KEY","VALUE");
+
+    while (next_count != 0) {
+        next_count = (*(unique_array+i)).id_count;
+        if (next_count > 1){
+            strcat(value, "{");
+            strcat(value, ((*(unique_array+i)).next[0]));
+
+            for (int j = 1; j < next_count; j++){
+                    strcat(value, ", ");
+                    strcat(value, ((*(unique_array+i)).next[j]));
+            }
+            strcat(value, "}");
+            printf("%200s%20.200s\n",((*(unique_array+i)).text), value);
+
+        }else{
+            printf("%200s%20.200s\n",((*(unique_array+i)).text),((*(unique_array+i)).next[0]));
+        }
+        i+=1;
+    }
 }
 
 void menu(){
@@ -123,7 +145,6 @@ void main(){
   unique_array = (struct data*)malloc(100000*sizeof(struct data));
   trans_array= (struct string*)malloc(100000*sizeof(struct string));
   next_array= (struct string*)malloc(100000*sizeof(struct string));//tester array
-  next_array= (struct string*)malloc(100000*sizeof(struct string));
   char temp[char_max];
   char isLoop, isLoop2;
 
@@ -135,16 +156,12 @@ void main(){
   do{
     printf("Masukkan nilai N: ");
     scanf("%lf", &N);
-    if(N<=2){
+    if(N < 3){
         printf("Nilai N tidak valid!\n");
     }
-  }while(N<=2);
+  }while(N < 3);
 
   pemecah_kata(N,file_ref,trans_array,unique_array);
+  tabelngram(unique_array);
+}
 
-  
-}
-=======
-  pemecah_kata(N,file_ref,trans_array,unique_array);//next_array
-  
-}
